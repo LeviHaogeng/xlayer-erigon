0e684dafd719a0af3a410112d804cb9eee99d0ba
Barry <zjg555543@163.com>
2025-09-24 18:04:06 +0800

    Special block for mainnet upgrade (#786)

--- erigon-lib/chain/hardfork_xlayer.go
+++ erigon-lib/chain/hardfork_xlayer.go
@@ -25,7 +25,7 @@ type XLayerForkConfig struct {
 
 // Fork configurations
 var ForkId13DencunConfig = XLayerForkConfig{
-	MainnetBlock: 1000000000000, // TODO, need to be updated
+	MainnetBlock: 41191800,
 	TestnetBlock: 7953000,
 	DevnetBlock:  30,
 }
